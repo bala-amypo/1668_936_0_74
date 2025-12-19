1e5fa0934da5d778b57b9fe730f4a793921a30f4
bala-amypo <bala@amypo.in>
2025-12-19 10:07:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentDetailsEntity.java
+++ src/main/java/com/example/demo/entity/StudentDetailsEntity.java
@@ -16,7 +16,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class StudentDetailsEntity{
     @Id
-
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
