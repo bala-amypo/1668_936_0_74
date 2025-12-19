73100b10543c58654e7fa8db05d7d7afe571bb99
bala-amypo <bala@amypo.in>
2025-12-19 06:37:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -6,13 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Date;
-import lombok.Getter;
-import lombok.  
+import lombok.Data;
+import lombok.NoArgsConstructor;  
 
 
 @Entity
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentEntity{
     @Id
 
@@ -23,44 +24,4 @@ public class StudentEntity{
     private String password;
     private Date createAt;
 
-    public void setId(Integer id){
-        this.id=id;
-    }
-    public Integer getId(){
-        return id;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setCreateAt(Date createAt){
-        this.createAt=createAt;
-    }
-    public Date getCreateAt(){
-        return createAt;
-    }
-    public StudentEntity(){
-
-    }
-    public StudentEntity(Integer id,String name,String email,String password,Date createAt){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
-        this.createAt=createAt;
-    }
 }
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -11,74 +11,24 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;  
+
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class ValidationEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @NotNull
-    @Size(min=2,max=10,message="must be 2 to 10 character")
     private String username;
-    @Email(message="Email is not valid")
     private String email;
-    @Size(min=7,max=15,message="must be 7 to 15 character")
-    @NotNull(message="Password is mandatory")
     private String password;
-    @Max(30)
-    @Positive(message="Age must be positive")
     private int age;
 
-    public void setId(Long id){
-        this.id=id;
-    }
-    public Long getId(){
-        return id;
-    }
-    public void setUsername(String username){
-        this.username=username;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setAge(int Age){
-        this.age=Age;
-    }
-    public int getAge(){
-        return age;
-    }
-    public ValidationEntity(Long id,
-    @NotNull
-    @Size(min=2,max=10,message="must be 2 to 10 character")
-    String username,
-    @Email(message="Email is not valid")
-    String email,
-    @Size(min=7,max=15,message="must be 7 to 15 character")
-    @NotNull(message="Password is mandatory")
-    String password,
-    @Max(30)
-    @Positive(message="Age must be positive")
-    int age
-){
-      this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.age=age;
-    }
-    public ValidationEntity(){
-    
-    }     
 }
